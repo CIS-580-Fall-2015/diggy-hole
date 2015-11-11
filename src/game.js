ecc4697ff2994651052c9eaa12e194fe776f1a51
Wyatt Watson <wyattw@ksu.edu>
2015-11-10 18:27:52 -0600

    Update game.js

--- src/game.js
+++ src/game.js
@@ -18,6 +18,7 @@ module.exports = (function (){
 	  DynamiteDwarf = require('./dynamiteDwarf.js'),
 	  Kakao = require('./Kakao.js'),
 	  kakao,
+      GoblinMiner = require('./goblin-miner.js'),
       player,
       screenCtx,
       backBuffer,
