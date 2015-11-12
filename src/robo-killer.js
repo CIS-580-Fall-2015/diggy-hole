70ac77f630e08f36aba24639046ee1bd571c6f8f
Christian Hughes <ChristianHughes@jardine-81-13-81-41.reshall.ksu.edu>
2015-11-11 23:54:03 -0600

    Added sprite sheets and Entity Module

---
+++ src/robo-killer.js
@@ -0,0 +1,79 @@
+/* Entity: robo-killer module
+ * Implements the entity pattern, provides specific robo-killer constructs.
+ *
+ * Author: Christian Hughes
+ *
+ * Image labled for reuse. Source: http://4.bp.blogspot.com/-iu9fsSz_L5I/T9X-dz3AFhI/AAAAAAAAAfQ/bG6hvI_eKzo/s1600/robotboy.png
+ */
+module.exports = (function(){
+
+  /* Constructor
+   * Generally speaking, you'll want to set
+   * the X and Y position, as well as the layerX
+   * of the map the entity is located on
+   */
+  function Entity(locationX, locationY, mapLayer){
+    this.x = locationX;
+    this.y = locationY;
+    this.mapLayer = mapLayer;
+  }
+
+  /* Update function
+   * parameters:
+   * - elapsedTime is the time that has passed since the
+   *   previous frame
+   * - tilemap is the currently loaded tilemap; you'll
+   *   probably want to call its tileAt and setTile methods.
+   * - entityManager is the game's entity manager, and
+   *   keeps track of where all game entities are.
+   *   you can call its query functions
+   */
+  Entity.prototype.update = function(elapsedTime, tilemap, entityManager) {
+      // TODO: Determine what your entity will do
+  }
+
+  /* Render function
+   * parameters:
+   *  - context is the rendering context.  It may be transformed
+   *    to account for the camera
+   */
+   Entity.prototype.render = function(context) {
+     // TODO: Draw your entity sprite
+   }
+
+   /* Collide function
+    * This function is called by the entityManager when it determines
+    * a possible collision.
+    * parameters:
+    * - otherEntity is the entity this enemy collided with
+    *   You will likely want to use
+    *     'otherEntity instanceof <Type>'
+    *   to determine what type it is to know what to
+    *   do with it.
+    */
+   Entity.prototype.collide = function(otherEntity) {
+   }
+
+   /* BoundingBox function
+    * This function returns an axis-aligned bounding
+    * box, i.e {top: 0, left: 0, right: 20, bottom: 50}
+    * the box should contain your entity or at least the
+    * part that can be collided with.
+    */
+   Entity.prototype.boundingBox = function() {
+     // Return a bounding box for your entity
+   }
+
+   /* BoundingCircle function
+    * This function returns a bounding circle, i.e.
+    * {cx: 0, cy: 0, radius: 20}
+    * the circle should contain your entity or at
+    * least the part that can be collided with.
+    */
+   Entity.prototype.boundingCircle = function() {
+     // Return a bounding circle for your entity
+   }
+
+   return Entity;
+
+}());
