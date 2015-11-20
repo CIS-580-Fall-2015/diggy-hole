5d043a154662b826b8fc3f83c7f48ae5a7e37ee4
Kyle Brown <kbrown2193@yahoo.com>
2015-11-20 15:02:38 -0600

    Added Background music

--- src/game.js
+++ src/game.js
@@ -172,6 +172,14 @@ module.exports = (function (){
     blobber = new Blobber(280,240,0,0,0,player,extantBlobbers);
     entityManager.add(blobber);
 
+			
+	// Kyle Brown: Background Music
+	var bgMusic = new Audio('./resources/sounds/DiggyHoleBGMusicAm.wav');
+	   bgMusic.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+	}, false);
+	bgMusic.play();
 
 
   };
