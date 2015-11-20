2097184c59617bce46af3c5e38ff24694b870afd
dubenale <dubenale@ksu.edu>
2015-11-20 01:31:01 -0600

    getPlayer bug removed

--- diggy.js
+++ diggy.js
@@ -2961,7 +2961,7 @@ module.exports = (function() {
 
   function getPlayer() {
     for (var i = 0; i < entityCount; i++) {
-      if (entities[i] && entities[i] instanceof Player) {
+      if (entities[i] && entities[i].type == "player") {
         return entities[i];
       }
     }
