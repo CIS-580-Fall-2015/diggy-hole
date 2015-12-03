39bfacb3874965e7fedd5abcfc42b97690e186f0
Wyatt Watson <wyattw@ksu.edu>
2015-12-02 19:36:24 -0600

    Changed tile value for removeTileAt

--- src/tilemap.js
+++ src/tilemap.js
@@ -735,7 +735,7 @@ module.exports = (function (){
   var removeTileAt = function(x, y, layer) {
     if(layer < 0 || x < 0 || y < 0 || layer >= layers.length || x > mapWidth || y > mapHeight)
       return undefined;
-    layers[layer].data[x + y*mapWidth] =  16;
+    layers[layer].data[x + y*mapWidth] =  8;
   }
 
   //return current tile layer, 0: sky, 1: crust 2: magma
