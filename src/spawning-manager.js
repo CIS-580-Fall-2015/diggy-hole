78d27fdc9fba83d4945f20151067366f783ef744
Wyatt Watson <wyattw@ksu.edu>
2015-12-16 21:07:39 -0600

    Update spawning-manager.js

--- src/spawning-manager.js
+++ src/spawning-manager.js
@@ -8,18 +8,21 @@ module.exports = (function() {
 	
 	var birdData = {
 		Entity: require('./bird.js'),
+		type: 'bird',
 		limit: 8,
 		count: 0
 	};
 	
 	var turretData = {
 		Entity: require('./turret.js'),
+		type: 'turret',
 		limit: 3,
 		count: 0
 	};
 			
 	var barrelData = {
 		Entity: require('./barrel.js'),
+		type: 'Barrel',
 		limit: 15,
 		count: 0
 	};
@@ -30,18 +33,21 @@ module.exports = (function() {
 	
 	var demonGHogData = {
 		Entity: require('./DemonicGroundH.js'),
+		type: 'DemonicGroundHog',
 		limit: 10,
 		count: 0
 	};
 	
 	var goblinMinerData = {
 		Entity: require('./goblin-miner.js'),
+		type: 'goblinMiner',
 		limit: 8,
 		count: 0
 	};
 	
 	var goblinShamanData = {
 		Entity: require('./goblin-shaman.js'),
+		type: 'shaman',
 		limit: 5,
 		count: 0
 	};
@@ -52,12 +58,14 @@ module.exports = (function() {
 	
 	var ghostMinerData = {
 		Entity: require('./ghostminer.js'),
+		type: 'ghost',
 		limit: 4,
 		count: 0
 	};
 	
 	var dynamiteDwarfData = {
 		Entity: require('./dynamiteDwarf.js'),
+		type: 'dynamiteDwarf',
 		limit: 2,
 		count: 0
 	}
@@ -144,7 +152,7 @@ module.exports = (function() {
         this.updateTimeLeft = 0;
     }
 	
-	var updatePeriodSeconds = 2;
+	var updatePeriodSeconds = .1;
 
     SpawningManager.prototype.update = function (elapsedTime, tilemap) {
         this.updateTimeLeft -= elapsedTime;
@@ -182,6 +190,14 @@ module.exports = (function() {
 				var num = Math.floor(Math.random()*3);
 				if( tile && tile.data) {
 					if (tile.data.type == "SkyBackground" || tile.data.type == "Clouds") {
+						//Update count of that one
+						skyEntities[num].count = 0;
+						var ents = this.entityManager.queryRadius(this.player.x, this.player.y, 75*64);
+						for(var i = 0; i < ents.length; i++){
+							if(ents[i].type == skyEntities[num].type){
+								skyEntities[num].count++;
+							}
+						}
 						if (skyEntities[num].limit > skyEntities[num].count) {
 							this.entityManager.add(
 								new skyEntities[num].Entity(x * 64, y * 64, 0)
@@ -191,6 +207,13 @@ module.exports = (function() {
 						else
 						{
 							num = Math.floor(Math.random()*5);
+							powerUps[num].count = 0;
+							var ents = this.entityManager.queryRadius(this.player.x, this.player.y, 75*64);
+							for(var i = 0; i < ents.length; i++){
+								if(ents[i].type == powerUps[num].name){
+									powerUps[num].count++;
+								}
+							}
 							if(powerUps[num].limit > powerUps[num].count){
 								this.entityManager.add(new powerUps[num].Entity(x*64, y*64, 0,powerUps[num].name, powerUps[num].width, powerUps[num].height, powerUps[num].frameNum, powerUps[num].img, powerUps[num].flying, powerUps[num].duration));
 								powerUps[num].count++;
@@ -198,6 +221,14 @@ module.exports = (function() {
 						}
 					} 
 					else if(tile.data.type == "CaveBackground" || tile.data.type == "Water"){
+						//Update count of that one
+						middleEntities[num].count = 0;
+						var ents = this.entityManager.queryRadius(this.player.x, this.player.y, 75*64);
+						for(var i = 0; i < ents.length; i++){
+							if(ents[i].type == middleEntities[num].type){
+								middleEntities[num].count++;
+							}
+						}
 						if(middleEntities[num].limit > middleEntities[num].count){
 							this.entityManager.add(
 									new middleEntities[num].Entity(x*64, y*64, 0)
@@ -207,6 +238,13 @@ module.exports = (function() {
 						else
 						{
 							num = Math.floor(Math.random()*5);
+							powerUps[num].count = 0;
+							var ents = this.entityManager.queryRadius(this.player.x, this.player.y, 75*64);
+							for(var i = 0; i < ents.length; i++){
+								if(ents[i].type == powerUps[num].name){
+									powerUps[num].count++;
+								}
+							}
 							if(powerUps[num].limit > powerUps[num].count){
 								this.entityManager.add(new powerUps[num].Entity(x*64, y*64, 0,powerUps[num].name, powerUps[num].width, powerUps[num].height, powerUps[num].frameNum, powerUps[num].img, powerUps[num].flying, powerUps[num].duration));
 								powerUps[num].count++;
@@ -214,6 +252,14 @@ module.exports = (function() {
 						}
 					}
 					else if(tile.data.type == "Lava" || tile.data.type == "DarkBackground" || tile.data.type == "DugBackground"){
+						//Update count of that one
+						deepEntities[num].count = 0;
+						var ents = this.entityManager.queryRadius(this.player.x, this.player.y, 75*64);
+						for(var i = 0; i < ents.length; i++){
+							if(ents[i].type == deepEntities[num].type){
+								deepEntities[num].count++;
+							}
+						}
 						if(deepEntities[num].limit > deepEntities[num].count){
 							this.entityManager.add(
 									new deepEntities[num].Entity(x*64, y*64, 0)
@@ -224,6 +270,13 @@ module.exports = (function() {
 						else
 						{
 							num = Math.floor(Math.random()*5);
+							powerUps[num].count = 0;
+							var ents = this.entityManager.queryRadius(this.player.x, this.player.y, 75*64);
+							for(var i = 0; i < ents.length; i++){
+								if(ents[i].type == powerUps[num].name){
+									powerUps[num].count++;
+								}
+							}
 							if(powerUps[num].limit > powerUps[num].count){
 								this.entityManager.add(new powerUps[num].Entity(x*64, y*64, 0,powerUps[num].name, powerUps[num].width, powerUps[num].height, powerUps[num].frameNum, powerUps[num].img, powerUps[num].flying, powerUps[num].duration));
 								powerUps[num].count++;
